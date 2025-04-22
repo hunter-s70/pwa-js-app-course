368d42fc9fb4f401496c7c4cb2512a9ae2b30165
hunter-s70 <serg.buhtoyarov@gmail.com>
2025-04-22 17:02:47 +0300

    Register service worker and handle basic lifecycle events

--- public/sw.js
+++ public/sw.js
@@ -1,23 +1,31 @@
 self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('first-app')
-      .then(function(cache) {
-        cache.addAll([
-          '/',
-          '/index.html',
-          '/src/css/app.css',
-          '/src/js/app.js'
-        ])
-      })
-  );
+  console.log('installing SW...', event);
+  // event.waitUntil(
+  //   caches.open('first-app')
+  //     .then(function(cache) {
+  //       cache.addAll([
+  //         '/',
+  //         '/index.html',
+  //         '/src/css/app.css',
+  //         '/src/js/app.js'
+  //       ])
+  //     })
+  // );
+  // return self.clients.claim();
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('activating SW...', event);
   return self.clients.claim();
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(res) {
-        return res;
-      })
-  );
+  console.log('fetching Data...', event);
+  event.respondWith(fetch(event.request));
+  // event.respondWith(
+  //   caches.match(event.request)
+  //     .then(function(res) {
+  //       return res;
+  //     })
+  // );
 });
