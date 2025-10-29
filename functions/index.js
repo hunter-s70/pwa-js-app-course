5d763d0359dbc102695587cb21c7d35f339239d1
hunter-s70 <8670953+hunter-s70@users.noreply.github.com>
2025-10-29 23:15:57 +0200

    Get image on the server and store in the firebase bucket

--- functions/index.js
+++ functions/index.js
@@ -2,12 +2,22 @@ var functions = require('firebase-functions');
 var admin = require('firebase-admin');
 var cors = require('cors')({origin: true});
 var webpush = require('web-push');
+var formidable = require('formidable');
+var fs = require('fs');
+var UUID = require('uuid-v4');
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 
 var serviceAccount = require("./pwagram-fb-key.json");
+
+var gcconfig = {
+  projectId: 'pwagram-99adf',
+  keyFilename: 'pwagram-fb-key.json',
+};
+
+var gcs = require('@google-cloud/storage')(gcconfig);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -16,46 +26,68 @@ admin.initializeApp({
 
 exports.storePostData = functions.https.onRequest(function (request, response) {
   cors(request, response, function () {
-    // Save new post to the database
-    admin.database().ref('posts').push({
-      id: request.body.id,
-      title: request.body.title,
-      location: request.body.location,
-      image: request.body.image
-    })
-      .then(function () {
-        // Setup VAPID
-        webpush.setVapidDetails(
-          'mailto:business@academind.com',
-          'BKapuZ3XLgt9UZhuEkodCrtnfBo9Smo-w1YXCIH8YidjHOFAU6XHpEnXefbuYslZY9vtlEnOAmU7Mc-kWh4gfmE', // Public key
-          'AyVHwGh16Kfxrh5AU69E81nVWIKcUwR6a9f1X4zXT_s', // Private key
-        );
-        return admin.database().ref('subscriptions').once('value');
-      })
-      .then(function (subscriptions) {
-        subscriptions.forEach(function (sub) {
-          var pushConfig = {
-            endpoint: sub.val().endpoint,
-            keys: {
-              auth: sub.val().keys.auth,
-              p256dh: sub.val().keys.p256dh
-            }
-          };
+    var uuid = UUID();
+    var formData = new formidable.IncomingForm();
+    formData.parse(request, function (err, fields, files) {
+      fs.rename(files.file.path, '/tmp/' + files.file.name);
 
-          // Send notification after creating a new post
-          webpush.sendNotification(pushConfig, JSON.stringify({
-            title: 'New Post',
-            content: 'New Post added!',
-            openUrl: '/help',
-          }))
-            .catch(function(err) {
-              console.log(err);
+      var bucket = gcs.bucket('pwagram-99adf.appspot.com');
+
+      bucket.upload('/tmp/' + files.file.name, {
+        uploadType: 'media',
+        metadata: {
+          metadata: {
+            contentType: files.file.type,
+            firebaseStorageDownloadTokens: uuid,
+          }
+        }
+      }, function (err, file) {
+        if (!err) {
+          // Save new post to the database
+          admin.database().ref('posts').push({
+            id: fields.id,
+            title: fields.title,
+            location: fields.location,
+            image: 'https://firebasestorage.googleapis.com/v0/b/' + bucket.name + '/o/' + encodeURIComponent(file.name) + '?alt=media&token=' + uuid
+          })
+            .then(function () {
+              // Setup VAPID
+              webpush.setVapidDetails(
+                'mailto:business@academind.com',
+                'BKapuZ3XLgt9UZhuEkodCrtnfBo9Smo-w1YXCIH8YidjHOFAU6XHpEnXefbuYslZY9vtlEnOAmU7Mc-kWh4gfmE', // Public key
+                'AyVHwGh16Kfxrh5AU69E81nVWIKcUwR6a9f1X4zXT_s', // Private key
+              );
+              return admin.database().ref('subscriptions').once('value');
             })
-        });
-        response.status(201).json({message: 'Data stored', id: request.body.id});
-      })
-      .catch(function (err) {
-        response.status(500).json({error: err});
+            .then(function (subscriptions) {
+              subscriptions.forEach(function (sub) {
+                var pushConfig = {
+                  endpoint: sub.val().endpoint,
+                  keys: {
+                    auth: sub.val().keys.auth,
+                    p256dh: sub.val().keys.p256dh
+                  }
+                };
+
+                // Send notification after creating a new post
+                webpush.sendNotification(pushConfig, JSON.stringify({
+                  title: 'New Post',
+                  content: 'New Post added!',
+                  openUrl: '/help',
+                }))
+                  .catch(function(err) {
+                    console.log(err);
+                  })
+              });
+              response.status(201).json({message: 'Data stored', id: fields.id});
+            })
+            .catch(function (err) {
+              response.status(500).json({error: err});
+            });
+        } else {
+          console.log(err);
+        }
       });
+    });
   });
 });
